04596fa3b9fc1fe08c6b7fc5ccc50c78bb58a003
anandgarikapati1 <anand.g@financebox.in>
2024-11-04 18:46:35 +0530

    dev issue

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${secondary_font.variable}`}>
       <body>
+        <h1>mek</h1>
         <Preloader />
         {children}
       </body>
